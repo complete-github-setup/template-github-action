457483cd8fe4f18ab4effcb51fd9e49a8c096057
Flemming Behrend <topfeb@gmail.com>
2022-07-28 23:04:48 +0200

    [skip actions]

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,11 +2,34 @@ import * as core from '@actions/core';
 import { run } from './main';
 
 describe('Test for main', () => {
-  jest.spyOn(core, 'info');
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('should print with core.info', async () => {
+    const infoMock = jest.spyOn(core, 'info');
     await run();
-    expect(core.info).toBeCalledTimes(1);
-    expect(core.info).toHaveBeenCalledWith('The action is running');
+    expect(infoMock).toBeCalledTimes(1);
+    expect(infoMock).toHaveBeenCalledWith('The action is running');
+  });
+
+  it('should print with core.setFailed if Error', async () => {
+    const FailedMock = jest.spyOn(core, 'setFailed');
+    jest.spyOn(core, 'info').mockImplementation(() => {
+      throw new Error('error message');
+    });
+    await run();
+    expect(FailedMock).toBeCalledTimes(1);
+    expect(FailedMock).toHaveBeenCalledWith('error message');
+  });
+
+  it('should print with core.error if not Error', async () => {
+    const errorMock = jest.spyOn(core, 'error');
+    jest.spyOn(core, 'info').mockImplementation(() => {
+      throw 'error message';
+    });
+    await run();
+    expect(errorMock).toBeCalledTimes(1);
+    expect(errorMock).toHaveBeenCalledWith('Unexpected execution flow. Error was thrown that was not of type Error');
   });
 });
